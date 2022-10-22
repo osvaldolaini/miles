8082a1679658a34a88c80c72af6c65db69f13834
Osvaldo Laini <osvaldolaini@hotmail.com>
2022-10-21 21:45:23 -0300

    master

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# miles
-Sistema de facilitação de troca de milhas
+# default
+
+# list
+    - Laravel 8x
+    - Breeze 1.9.2
+    - Font awesome
+    - Socialite
+    - Socialite buttons
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,35 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+#region Socialite
+Route::get('/login/{driver}/redirect', function ($driver) {
+    return Socialite::driver($driver)->redirect();
+})->name('auth.social.redirect');
+
+Route::get('/auth/callback', function ($driver) {
+    $socialUser=Socialite::driver($driver)->stateless->user();
+    $socialUser = User::updateOrCreate([
+        'email' => $socialUser->email,
+    ], [
+        'name' => $socialUser->name,
+        'email' => $socialUser->email,
+        'github_token' => $socialUser->token,
+        'github_refresh_token' => $socialUser->refreshToken,
+    ]);
+
+    Auth::login($socialUser);
+
+    return redirect('/dashboard');
+});
+
+#endregion
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';

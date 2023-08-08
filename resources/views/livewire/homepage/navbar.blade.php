011666d4668bff5e395fca2e35b764e01a2cbc8c
LaiGuz <laiguz@hotmail.com>
2023-08-07 21:29:00 -0300

    master

--- resources/views/livewire/homepage/navbar.blade.php
+++ resources/views/livewire/homepage/navbar.blade.php
@@ -102,80 +102,66 @@ px-2 py-2 navbar-expand-lg bg-gray-900"
     </div>
     <div class="w-full items-center justify-end md:justify-between" :class="{ 'flex': open, 'hidden': !open }">
         <ul class="flex w-full flex-col list-none items-center">
-            <li class="inline-block relative">
-                <div class="bg-gray-900  float-left
-            py-2 list-none text-center text-bold text-teal-500 min-w-48"
-                    id="demo-pages-dropdown">
-                    @auth
-                        <a href="{{ url('/app') }}"
-                            class="text-sm py-2 px-4
-                    font-bold block w-full whitespace-nowrap bg-transparent
-                    text-bold text-teal-500 ">
-                            Home
-                        </a>
-                        <a href="{{ url('/app') }}"
-                            class="text-sm py-2 px-4
-                    font-bold block w-full whitespace-nowrap bg-transparent
-                    text-bold text-teal-500 ">
-                            Chat
-                        </a>
-                        <a href="{{ url('/meus-pedidos') }}"
-                            class="text-sm py-2 px-4
-                    font-bold block w-full whitespace-nowrap bg-transparent
-                    text-bold text-teal-500 ">
-                            Meus pedidos
-                        </a>
-                        <a href="{{ url('/minhas-ofertas') }}"
-                            class="text-sm py-2 px-4
-                    font-bold block w-full whitespace-nowrap bg-transparent
-                    text-bold text-teal-500 ">
-                            Minhas ofertas
-                        </a>
-
-                        <a href="{{ route('profile.user') }}" class="text-sm py-2 px-4
-                        font-bold block w-full whitespace-nowrap bg-transparent
-                        text-bold text-teal-500 ">
-                        Meu cadastro
-                    </a>
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}" x-data>
-                            @csrf
-
-                            <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    @else
-                        <div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
-                            id="demo-pages-dropdown">
-                            <a href="{{ route('login') }}"
-                                class="bg-teal-500
-                    hover:bg-gray-900 border-2 border-teal-500
-                    active:bg-teal-300 text-white text-xs font-bold uppercase px-6 py-2.5 rounded-full shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-4 ease-linear transition-all duration-150">
-                                Entrar
-                            </a>
-                            <a href="{{ route('register') }}"
-                                class="
-                    border-2 border-teal-500 bg-gray-900 text-white
-                    hover:bg-teal-500
-                    active:bg-teal-300 active:text-white text-xs font-bold uppercase px-6 py-2.5 rounded-full shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-4 ease-linear transition-all duration-150">
-                                Registrar
-                            </a>
-                        </div>
-                    @endauth
-
-                </div>
+            <li class="li dark:bg-gray-800 dark:text-gray-50 cursor-pointer">
+                <a href="#" class="a flex items-center p-2 space-x-3 rounded-md">
+                    <span class="span ">
+                        <svg class="w-4 h-4" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24"
+                            data-testid="HomeIcon">
+                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path>
+                        </svg>
+                    </span>
+                    <span class="span semibold">Home</span>
+                </a>
+            </li>
+            <li class="li dark:bg-gray-800 dark:text-gray-50 cursor-pointer">
+                <a href="#" class="a flex items-center p-2 space-x-3 rounded-md">
+                    <span class="span ">
+                        <svg class="w-4 h-4" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24"
+                            data-testid="QuestionAnswerIcon">
+                            <path
+                                d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z">
+                            </path>
+                        </svg>
+                    </span>
+                    <span class="span semibold">Chat</span>
+                </a>
+            </li>
+            <li class="li dark:bg-gray-800 dark:text-gray-50 cursor-pointer">
+                <a href="{{ route('demand.user') }}" class="a flex items-center p-2 space-x-3 rounded-md">
+                    <span class="span ">
+                        <svg class="w-4 h-4" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24"
+                            data-testid="BookmarkRemoveIcon">
+                            <path
+                                d="M21 7h-6V5h6v2zm-2 3.9c-.32.07-.66.1-1 .1-2.76 0-5-2.24-5-5 0-1.13.37-2.16 1-3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V10.9z">
+                            </path>
+                        </svg>
+                    </span>
+                    <span class="span semibold">Meus pedidos</span>
+                </a>
+            </li>
+            <li class="li dark:bg-gray-800 dark:text-gray-50 cursor-pointer">
+                <a href="#" class="a flex items-center p-2 space-x-3 rounded-md">
+                    <span class="span ">
+                        <svg class="w-4 h-4" fill="currentColor" focusable="false" aria-hidden="true" viewBox="0 0 24 24"
+                            data-testid="BookmarkAddIcon">
+                            <path
+                                d="M21 7h-2v2h-2V7h-2V5h2V3h2v2h2v2zm-2 14-7-3-7 3V5c0-1.1.9-2 2-2h7c-.63.84-1 1.87-1 3 0 2.76 2.24 5 5 5 .34 0 .68-.03 1-.1V21z">
+                            </path>
+                        </svg>
+                    </span>
+                    <span class="span semibold">Minhas ofertas</span>
+                </a>
             </li>
             @if (Route::has('login'))
                 <div class="top-0 right-0 px-6 py-5 sm:block">
                     <li class="flex items-center">
                         @auth
                             <div class="flex items-center mr-5">
-                                {{-- <a href="{{ route('demand.create') }}" class="bg-teal-500
+                                <a href="{{ route('demand') }}" class="bg-teal-500
                                 hover:bg-gray-900 border-2 border-teal-500
                                 active:bg-teal-300 text-white text-xs font-bold uppercase px-6 py-2.5 rounded-full shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-4 ease-linear transition-all duration-150" >
                                 FAZER PEDIDO <span class="fa-solid fa-plus"></span>
-                                </a> --}}
+                                </a>
                             </div>
                             {{-- <div class="items-center mr-5 hidden lg:flex">
                                 <x-dropdown-alerts></x-dropdown-user>

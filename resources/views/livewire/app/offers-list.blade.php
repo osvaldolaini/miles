9014c2e6d720dc462b7d7b1d47ef7434ccadea59
LaiGuz <laiguz@hotmail.com>
2023-08-09 20:23:55 -0300

    master

--- resources/views/livewire/app/offers-list.blade.php
+++ resources/views/livewire/app/offers-list.blade.php
@@ -29,7 +29,7 @@
                                     d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z">
                                 </path>
                             </svg>
-                            Negociar
+                            Conversar
                         </a>
                         <a class="btn btn-info btn-sm" href="{{ route('offer.user', [$offer->code]) }}">
                             <svg class="w-5 h-5" fill="currentColor" focusable="false" aria-hidden="true"

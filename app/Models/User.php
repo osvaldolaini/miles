fd226ac783fe7c69079b12a38baf5a83f0d08a27
LaiGuz <laiguz@hotmail.com>
2023-08-15 20:15:00 -0300

    master

--- app/Models/User.php
+++ app/Models/User.php
@@ -114,12 +114,12 @@ class User extends Authenticatable
                 $kk = explode('.',$k);
                 $m =  $kk[0] . 'K';
                 break;
-                case $k >= 1000000 && $k < 1000000000:
+                case $k >= 100000 && $k < 100000000:
                     $k = $k / 1000;
                     $kk = explode('.',$k);
                     $m =  $kk[0] . 'M';
                     break;
-                case $k > 1000000000:
+                case $k > 100000000:
                         $k = $k / 1000;
                         $kk = explode('.',$k);
                         $m =  $kk[0] . 'B';
@@ -143,12 +143,12 @@ class User extends Authenticatable
                 $kk = explode('.',$k);
                 $m =  $kk[0] . 'K';
                 break;
-                case $k >= 1000000 && $k < 1000000000:
+                case $k >= 100000 && $k < 100000000:
                     $k = $k / 1000;
                     $kk = explode('.',$k);
                     $m =  $kk[0] . 'M';
                     break;
-                case $k > 1000000000:
+                case $k > 100000000:
                         $k = $k / 1000;
                         $kk = explode('.',$k);
                         $m =  $kk[0] . 'B';

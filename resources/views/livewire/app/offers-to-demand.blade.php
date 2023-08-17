b411f0724c42257f096e6477171fde0aa7bd4e02
LaiGuz <laiguz@hotmail.com>
2023-08-17 15:34:18 -0300

    master

--- resources/views/livewire/app/offers-to-demand.blade.php
+++ resources/views/livewire/app/offers-to-demand.blade.php
@@ -30,7 +30,7 @@
                     </div>
                 </div>
             </label>
-            <div tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-80">
+            <div tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-100">
                 <div class="py-2">
                     @foreach ($offers->take(3) as $offer)
                         @if ($linkOffer == true)
@@ -93,14 +93,9 @@
                                 </div>
                                 <div class="stat p-1 border-none sm:text-right">
                                     <div class="stat-title py-0 my-0">
-                                        <button class="btn btn-outline btn-success btn-sm">
-                                            <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
-                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M10 20C4.486 20 0 15.514 0 10S4.486 0 10 0s10 4.486 10 10-4.486 10-10 10zm1-15a1 1 0 10-2 0v.17A3 3 0 007 8c0 1.013.36 1.77 1.025 2.269.54.405 1.215.572 1.666.685l.066.016c.55.138.835.224 1.018.361.085.064.225.182.225.669a1 1 0 01-.984 1 1.611 1.611 0 01-.325-.074 2.533 2.533 0 01-.984-.633 1 1 0 00-1.414 1.414A4.548 4.548 0 009 14.804V15a1 1 0 102 0v-.17A3 3 0 0013 12c0-1.013-.36-1.77-1.025-2.269-.54-.405-1.215-.572-1.666-.685l-.066-.016c-.55-.138-.835-.224-1.018-.361C9.14 8.605 9 8.487 9 8a1 1 0 01.984-1 1.618 1.618 0 01.325.074c.245.081.606.255.984.633a1 1 0 101.414-1.414A4.547 4.547 0 0011 5.196V5z" />
-                                            </svg>
+                                        <span class="btn btn-outline btn-success btn-sm">
                                             {{ $offer->value }}
-                                        </button>
+                                        </span>
                                     </div>
                                 </div>
                             </div>

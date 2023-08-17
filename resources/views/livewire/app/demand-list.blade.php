f2b7be69395e678667092e840e7bd9e711423273
LaiGuz <laiguz@hotmail.com>
2023-08-17 15:29:44 -0300

    master

--- resources/views/livewire/app/demand-list.blade.php
+++ resources/views/livewire/app/demand-list.blade.php
@@ -45,13 +45,13 @@
                         @if ($item->user->id != Auth::user()->id)
                             @livewire('app.demand-likes', ['demands' => $item], key($item->id))
                         @endif
-                        <button type="button" title="Compartilhar" class="mx-1 ">
+                        {{-- <button type="button" title="Compartilhar" class="mx-1 ">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 fill-current">
                                 <path
                                     d="M474.444,19.857a20.336,20.336,0,0,0-21.592-2.781L33.737,213.8v38.066l176.037,70.414L322.69,496h38.074l120.3-455.4A20.342,20.342,0,0,0,474.444,19.857ZM337.257,459.693,240.2,310.37,389.553,146.788l-23.631-21.576L215.4,290.069,70.257,232.012,443.7,56.72Z">
                                 </path>
                             </svg>
-                        </button>
+                        </button> --}}
                         {{-- dropdown ofertas --}}
                         @livewire('app.offers-to-demand', ['demand' => $item, 'linkOffer' => false], key($item->id))
                     </div>

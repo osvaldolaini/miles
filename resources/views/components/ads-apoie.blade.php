e30112f1bdba2e2fbbd3cf5c60873dbd5eb4bb13
LaiGuz <laiguz@hotmail.com>
2023-08-17 15:38:58 -0300

    master

--- resources/views/components/ads-apoie.blade.php
+++ resources/views/components/ads-apoie.blade.php
@@ -8,11 +8,11 @@
     </script>
     <div class="mx-0">
         <p >
-            PIX: <span class="select-all" >ats4slfi987ç3#</span>
+            PIX: <span class="select-all" >594e9d46-7f8c-4a70-a06e-45dfaa27e5ea</span>
              <button onclick="copyMeOnClipboard()" class="bg-teal-500 text-white border
             px-1 py-1 border-transparent rounded-md cursor-pointer">
             <i class="fa-regular fa-copy"></i> Copiar
         </p>
     </button>
-        <input type="hidden" value="ats4slfi987ç3#" class="copyMe"/>
+        <input type="hidden" value="594e9d46-7f8c-4a70-a06e-45dfaa27e5ea" class="copyMe"/>
     </div>

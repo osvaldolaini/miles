3a85baa34b10d43ae81fab552ee662582dfc7784
LaiGuz <laiguz@hotmail.com>
2023-08-21 22:08:50 -0300

    master

---
+++ resources/views/components/skeleton.blade.php
@@ -0,0 +1,86 @@
+<div wire:loading.inline>
+    <div
+        class="flex flex-col w-full gap-5 py-2 mt-2
+        mb-4 bg-teal-500 mx-auto shadow-lg select-none sm:p-4 sm:h-64 rounded-2xl sm:flex-row ">
+        <div class="mx-2 sm:mx-1 hidden sm:block bg-gray-200 h-52 sm:h-full sm:w-72 rounded-xl animate-pulse">
+        </div>
+        <div class="flex flex-col flex-1 gap-5 sm:p-2 mx-2 sm:mx-1">
+            <div class="flex flex-col flex-1 gap-3">
+                <div class="w-full bg-gray-200 animate-pulse h-14 rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+            </div>
+            <div class="flex gap-3 mt-auto">
+                <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                </div>
+                <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                </div>
+                <div class="w-20 h-8 ml-auto bg-gray-200 rounded-full animate-pulse">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div
+        class="flex flex-col w-full gap-5 py-2 mt-2
+        mb-4 bg-teal-500 mx-auto shadow-lg select-none sm:p-4 sm:h-64 rounded-2xl sm:flex-row ">
+        <div class="mx-2 sm:mx-1 hidden sm:block bg-gray-200 h-52 sm:h-full sm:w-72 rounded-xl animate-pulse">
+        </div>
+        <div class="flex flex-col flex-1 gap-5 sm:p-2 mx-2 sm:mx-1">
+            <div class="flex flex-col flex-1 gap-3">
+                <div class="w-full bg-gray-200 animate-pulse h-14 rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+            </div>
+            <div class="flex gap-3 mt-auto">
+                <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                </div>
+                <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                </div>
+                <div class="w-20 h-8 ml-auto bg-gray-200 rounded-full animate-pulse">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div
+        class="flex flex-col w-full gap-5 py-2 mt-2
+        mb-4 bg-teal-500 mx-auto shadow-lg select-none sm:p-4 sm:h-64 rounded-2xl sm:flex-row ">
+        <div class="mx-2 sm:mx-1 hidden sm:block bg-gray-200 h-52 sm:h-full sm:w-72 rounded-xl animate-pulse">
+        </div>
+        <div class="flex flex-col flex-1 gap-5 sm:p-2 mx-2 sm:mx-1">
+            <div class="flex flex-col flex-1 gap-3">
+                <div class="w-full bg-gray-200 animate-pulse h-14 rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+                <div class="w-full h-3 bg-gray-200 animate-pulse rounded-2xl">
+                </div>
+            </div>
+            <div class="flex gap-3 mt-auto">
+                <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                </div>
+                <div class="w-20 h-8 bg-gray-200 rounded-full animate-pulse">
+                </div>
+                <div class="w-20 h-8 ml-auto bg-gray-200 rounded-full animate-pulse">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

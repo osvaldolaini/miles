0c5f96dd9d3401af5bf1a8f59d806f33deb04d5b
LaiGuz <laiguz@hotmail.com>
2023-08-21 21:47:22 -0300

    master

--- resources/views/livewire/app/demand-list.blade.php
+++ resources/views/livewire/app/demand-list.blade.php
@@ -7,9 +7,9 @@
             <div
                 class="flex flex-col w-full gap-5 py-2 mt-2
                 mb-4 bg-teal-500 mx-auto shadow-lg select-none sm:p-4 sm:h-64 rounded-2xl sm:flex-row ">
-                <div class="bg-gray-200 h-52 sm:h-full sm:w-72 rounded-xl animate-pulse">
+                <div class="mx-2 sm:mx-1 bg-gray-200 h-52 sm:h-full sm:w-72 rounded-xl animate-pulse">
                 </div>
-                <div class="flex flex-col flex-1 gap-5 sm:p-2">
+                <div class="flex flex-col flex-1 gap-5 sm:p-2 mx-2 sm:mx-1">
                     <div class="flex flex-col flex-1 gap-3">
                         <div class="w-full bg-gray-200 animate-pulse h-14 rounded-2xl">
                         </div>

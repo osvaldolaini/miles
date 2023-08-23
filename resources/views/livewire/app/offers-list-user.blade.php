9187da12b71d8b34d2bd50834bb7e249a650f7d3
LaiGuz <laiguz@hotmail.com>
2023-08-23 17:02:46 -0300

    master

--- resources/views/livewire/app/offers-list-user.blade.php
+++ resources/views/livewire/app/offers-list-user.blade.php
@@ -60,7 +60,7 @@
                                     active:bg-green-300 text-white text-xs font-bold
                                     uppercase px-6 py-2.5 rounded-full
                                     shadow hover:shadow-md
-                                    outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-4
+                                    outline-none focus:outline-none ml-3 mb-4
                                     ease-linear transition-all duration-150">
                                         Finalizado
                                     </span>

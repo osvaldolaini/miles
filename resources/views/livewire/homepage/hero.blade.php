a5a0dcb1055008797cbd3c2b630f1408466c1440
guzenskicg <https://github.com/guzenskicg/miles.git>
2023-08-28 21:57:34 -0300

    Correção click modo retrato no celular.

--- resources/views/livewire/homepage/hero.blade.php
+++ resources/views/livewire/homepage/hero.blade.php
@@ -1,29 +1,37 @@
 <!-- component -->
 <section>
-	<div class="bg-black dark:bg-black text-white pb-20 pt-12">
+    <div class="bg-black dark:bg-black text-white pb-20 pt-12">
         <div class="container mx-auto flex flex-col md:flex-row items-center my-10 md:my-24">
             <div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
                 {{-- <h1 class="text-3xl md:text-5xl p-2 text-teal-500 tracking-loose">SisteMilhas</h1> --}}
                 <h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Encontre a melhor oportunidade
                 </h2>
-                <p class="text-sm md:text-base text-gray-50 mb-4">Explore essa nova formas de se conectar a pessoas para negociar milhas.</p>
+                <p class="text-sm md:text-base text-gray-50 mb-4">Explore essa nova formas de se conectar a pessoas para
+                    negociar milhas.</p>
                 <a href="{{ url('/explore') }}"
-                    class="bg-transparent hover:bg-teal-500 text-teal-500 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-teal-500 hover:border-transparent">
-                    Explore agora</a>
+                    class="
+                            border-2 border-teal-500 bg-gray-900 text-white
+                            hover:bg-teal-500
+                            active:bg-teal-300 active:text-white text-xs font-bold uppercase px-6 py-2.5 rounded-full shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-4 ease-linear transition-all duration-150">
+                    Explore agora
+                </a>
             </div>
             <div class="dark:bg-[000000] p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3 justify-center">
                 <div class="h-48 flex flex-wrap content-center">
                     <div>
-                        <img class="inline-block mt-28 hidden xl:block" src="{{ url('storage/images/hero/miles-hero-left.webp') }}">
+                        <img class="inline-block mt-28 hidden xl:block"
+                            src="{{ url('storage/images/hero/miles-hero-left.webp') }}">
                     </div>
                     <div>
-                        <img class="inline-block mt-24 md:mt-0 p-8 md:p-0"  src="{{ url('storage/images/hero/miles-hero-center.webp') }}">
+                        <img class="inline-block mt-24 md:mt-0 p-8 md:p-0"
+                            src="{{ url('storage/images/hero/miles-hero-center.webp') }}">
                     </div>
                     <div>
-                        <img class="inline-block mt-28 hidden lg:block" src="{{ url('storage/images/hero/miles-hero-right.webp') }}">
+                        <img class="inline-block mt-28 hidden lg:block"
+                            src="{{ url('storage/images/hero/miles-hero-right.webp') }}">
                     </div>
                 </div>
             </div>
         </div>
-	</div>
+    </div>
 </section>

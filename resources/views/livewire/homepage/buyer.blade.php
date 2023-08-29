cbf3e466d576d94c90b7a9962fdaac8f93446b8c
guzenskicg <https://github.com/guzenskicg/miles.git>
2023-08-28 22:52:20 -0300

    Corrigido as cores do modo dark no buyer e seller.

--- resources/views/livewire/homepage/buyer.blade.php
+++ resources/views/livewire/homepage/buyer.blade.php
@@ -5,9 +5,9 @@
                 COMPRADOR
             </p>
         </div>
-        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+        <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight dark:text-white sm:text-4xl md:mx-auto">
             <span class="relative inline-block">
-            <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+            <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
                 <defs>
                 <pattern id="b902cd03-49cc-4166-a0ae-4ca1c31cedba" x="0" y="0" width=".135" height=".30">
                     <circle cx="1" cy="1" r=".7"></circle>

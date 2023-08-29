06a020a65f5272d045b0bb6dd848422009a7bb41
guzenskicg <https://github.com/guzenskicg/miles.git>
2023-08-29 00:30:09 -0300

    Alterado margem links login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,22 +34,25 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                        href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 ml-1"
-                        href="{{ route('register') }}">
-                        {{ __('Not registered?') }}
-                    </a>
-                @endif
+            <div class="flex items-center justify-between mt-4">
+                <div>
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 ml-4"
+                            href="{{ route('register') }}">
+                            {{ __('Not registered?') }}
+                        </a>
+                    @endif
+                </div>
 
                 <x-button class="ml-4">
                     {{ __('Log in') }}
                 </x-button>
             </div>
+
         </form>
     </x-authentication-card>
 </x-guest-layout>

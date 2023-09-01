9affd2a7a1e498da2a0c9d4744d0b962163196b9
LaiGuz <laiguz@hotmail.com>
2023-08-31 21:45:14 -0300

    master

--- resources/views/livewire/homepage/faq.blade.php
+++ resources/views/livewire/homepage/faq.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <section class="bg-gray-800 text-gray-100">
+    {{-- <section class="bg-gray-800 text-gray-100">
         <div class="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
             <h2 class="text-teal-500 mb-12 text-4xl font-bold leadi text-center sm:text-5xl">Dúvidas frequentes</h2>
             <div class="divide-y divide-gray-700">
@@ -87,16 +87,107 @@
 
             </div>
         </div>
+    </section> --}}
+    <section class="bg-gray-800 text-gray-100">
+        <div class="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+            <h2 class="text-teal-500 mb-12 text-4xl font-bold leadi text-center sm:text-5xl">Dúvidas frequentes</h2>
+            <div class="divide-y divide-gray-700">
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        O que é o SisteMilhas?
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        O Sistemilhas é mais do que um sistema; é uma plataforma que veio para suprir as carências do
+                        mercado de comercialização de milhas, tais como: segurança, transparência e otimização.
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        Qual é o fator de segurança da plataforma?
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        A plataforma opera com verificação de e-mail para efetuar o login e traz, por meio de seu banco
+                        de dados, as informações necessárias para realizar uma comercialização segura.
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        Quanto custa?
+
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        ZERO REAIS! Durante o lançamento e a fase de adaptação, todos os nossos serviços estarão
+                        totalmente gratuitos. Após a consolidação, serão aplicadas taxas simbólicas que ajudarão na
+                        manutenção e no desenvolvimento da plataforma! Com certeza, valerá muito a pena!
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        E quanto aos grupos de Balcão dos quais já participo?
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        Você poderá continuar neles; além disso, terá a possibilidade de compartilhar seu pedido e
+                        perfil para que outros membros do grupo também possam acessar.
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        Quais são as vantagens?
+
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        Não limitado a um grupo de influencer ou curso
+                        Sistema anti-flood no chat e sem “fura fila” no privado
+                        Dados de uso (Notas, avaliações, Ofertas, Pedidos, Foto, Bio…)
+                        Reutilização de CPF
+                        Campo de Valor máximo no pedido
+                        Gerência de Emissões e Contas
+                        Histórico de Negociações, Pedidos e Ofertas
+                        E uma porção de melhorias de uma plataforma flexível em desenvolvimento!
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        Como funciona a reutilização de CPF?
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        Quando um usuário lança um pedido e informa o CPF do passageiro, caso você já tenha emitido
+                        passagens para esse CPF no mesmo programa de milhas, o pedido aparecerá com uma observação
+                        indicando que o CPF já foi utilizado.
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        Como faço para negociar ou compartilhar os dados da emissão?
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        Os detalhes podem ser tratados por meio do nosso chat, o qual é aberto pelo comprador
+                        exclusivamente para a oferta em questão. Isso evita o excesso de mensagens em sua página e
+                        impede que alguém "furte a fila" através de mensagens privadas.
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        Para quem o Sistemilhas é direcionado?
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        O Sistemilhas é direcionado principalmente a agentes de viagens ou proprietários de agências. No
+                        entanto, também pode ser usado por pessoas que comercializam milhas de maneira autônoma,
+                        independentemente do nível de conhecimento.
+                    </p>
+                </details>
+                <details class="w-full border rounded-lg">
+                    <summary class="px-4 py-6 focus:outline-none focus-visible:ri font-semibold md:col-span-5 text-teal-500 text-center sm:text-left">
+                        Como posso me manter informado sobre as atualizações da plataforma?
+                    </summary>
+                    <p class="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                        Temos um grupo aberto no Telegram, onde serão discutidas e compartilhadas atualizações de
+                        maneira transparente.
+                    </p>
+                </details>
+
+
+            </div>
+        </div>
     </section>
 </div>
-
-
-
-
-
-
-
-
-
-
-

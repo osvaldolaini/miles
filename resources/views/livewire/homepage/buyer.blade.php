bd026dc30e8aa4524279eb78462fe71dd0014409
LaiGuz <laiguz@hotmail.com>
2023-09-10 20:17:06 -0300

    master

--- resources/views/livewire/homepage/buyer.blade.php
+++ resources/views/livewire/homepage/buyer.blade.php
@@ -26,9 +26,9 @@
     </div>
     <div class="grid gap-10 lg:grid-cols-4 sm:grid-cols-2">
         <div>
-            <div class="flex items-center justify-between mb-6">
+            <div class="flex items-center justify-between mb-6 text-gray-700 dark:text-white">
             <p class="text-2xl font-bold">Passo 1</p>
-            <svg class="w-6 text-gray-700 dark:text-white transform rotate-90 sm:rotate-0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+            <svg class="w-6 transform rotate-90 sm:rotate-0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                 <line fill="none" stroke-miterlimit="10" x1="2" y1="12" x2="22" y2="12"></line>
                 <polyline fill="none" stroke-miterlimit="10" points="15,5 22,12 15,19 "></polyline>
             </svg>

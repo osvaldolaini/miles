c7d19b357b9637d6281c1629d76d4d8d1f45480b
LaiGuz <laiguz@hotmail.com>
2023-09-10 20:32:40 -0300

    master

--- resources/views/livewire/homepage/buyer.blade.php
+++ resources/views/livewire/homepage/buyer.blade.php
@@ -1,4 +1,4 @@
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+<div class="px-4 py-10 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
     <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
             <p class="inline-block p-3 mb-4 text-2xl sm:text-lg font-bold font-sans

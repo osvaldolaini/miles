3d8b7956bcbd76539fdd4f8c40f349a71e2e287f
LaiGuz <laiguz@hotmail.com>
2023-09-10 20:28:44 -0300

    master

--- resources/views/livewire/homepage/buyer.blade.php
+++ resources/views/livewire/homepage/buyer.blade.php
@@ -1,7 +1,8 @@
 <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
     <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
-            <p class="inline-block px-3 py-px mb-4 text-lg font-bold font-sans text-teal-700 uppercase rounded-full">
+            <p class="inline-block px-3 py-px mb-4 text-2xl sm:text-lg font-bold font-sans
+                text-teal-700 uppercase rounded-full">
                 COMPRADOR
             </p>
         </div>

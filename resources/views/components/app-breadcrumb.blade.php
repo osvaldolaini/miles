5252f5ddfed2258230a0b31f54a64a4fbe505682
LaiGuz <laiguz@hotmail.com>
2023-09-10 19:13:29 -0300

    master

--- resources/views/components/app-breadcrumb.blade.php
+++ resources/views/components/app-breadcrumb.blade.php
@@ -1,5 +1,6 @@
 @props(['filter' => false])
-<div class="w-full p-2 space-y-2 mt-5 lg:mt-0 bg-gray-100 dark:bg-gray-900 dark:text-gray-100 rounded-lg shadow-md">
+<div class="w-full p-2 space-y-2 mt-5 lg:mt-0 bg-gray-100
+            dark:bg-gray-800 dark:text-gray-100 rounded-lg shadow-md">
     <div class="flex items-center mx-auto container justify-between py-2">
         <div class="text-left">
             <h3 class="text-lg font-bold text-gray-900 dark:text-white">

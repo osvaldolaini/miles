8fd61d9c9f6c717b545c8a84e00b04f30bcaf863
LaiGuz <laiguz@hotmail.com>
2023-09-10 19:11:00 -0300

    master

--- resources/views/components/dialog-modal.blade.php
+++ resources/views/components/dialog-modal.blade.php
@@ -2,7 +2,7 @@
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }} :mt="$mt">
     <div class="px-6 py-4 bg-gray-100 text-gray-900  dark:bg-gray-900 dark:text-white">
-        <div class="text-lg font-medium text-left">
+        <div class="text-lg font-medium text-left dark:bg-gray-800">
             {{ $title }}
         </div>
 
@@ -11,7 +11,7 @@
         </div>
     </div>
 
-    <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 dark:bg-gray-900 text-right">
+    <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 dark:bg-gray-800 text-right">
         {{ $footer }}
     </div>
 </x-modal>
